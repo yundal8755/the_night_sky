1c7c862884b5107dde840146327c22e5679a7758
yundal <soneymarr@gmail.com>
2024-03-29 16:33:08 +0900

    chore: 로컬 에셋 경로 정의 모듈 추가
    - assets 파일명 수정
    - [AppAssets] : 로컬 Assets path를 관리하는 클래스

--- lib/app/constant/app_assets.dart
+++ lib/app/constant/app_assets.dart
@@ -1,20 +1,30 @@
+///
+/// 로컬 Assets path를 관리하는 클래스
+/// 
+
 abstract class AppAssets {
   //! icons
-  static const String replyDefault20 = 'assets/icons/message_pressed_20.svg';
-  static const String homeDefault32 = 'assets/icons/home_default_32.svg';
-  static const String homePressed32 = 'assets/icons/home_pressed_32.svg';
-  static const String postDefault32 = 'assets/icons/post_default_32.svg';
-  static const String micDefault40 = 'assets/icons/mic_default_40.svg';
-  static const String likeDefault24 = 'assets/icons/like_default_24.svg';
+  static const String appleLogo = 'assets/icons/apple_logo.svg';
+  static const String backDefault24 = 'assets/icons/back_default_24.svg';
   static const String chatDefault32 = 'assets/icons/chat_default_32.svg';
   static const String chatPressed32 = 'assets/icons/chat_pressed_32.svg';
-  static const String playDefault56 = 'assets/icons/play_default_56.svg';
-  static const String playDefault32 = 'assets/icons/play_default_32.svg';
-  static const String pauseDefault56 = 'assets/icons/pause_default_56.svg';
+  static const String googleLogo = 'assets/icons/google_logo.svg';
+  static const String homeDefault32 = 'assets/icons/home_default_32.svg';
+  static const String homePressed32 = 'assets/icons/home_pressed_32.svg';
+  static const String messageDefault32 = 'assets/icons/message_default_32.svg';
+  static const String micDefault56 = 'assets/icons/mic_default_56.svg';
   static const String pauseDefault32 = 'assets/icons/pause_default_32.svg';
+  static const String pauseDefault56 = 'assets/icons/pause_default_56.svg';
+  static const String playDefault32 = 'assets/icons/play_default_32.svg';
+  static const String playDefault56 = 'assets/icons/play_default_56.svg';
+  static const String seeMoreDefault24 = 'assets/icons/see_more_default_24.svg';
 
   //! Images
-  static const String basicProfileImage = 'assets/image/basicProfileImage.png';
-  static const String googleLogo = 'assets/image/google_logo.png';
-  static const String appleLogo = 'assets/image/apple_logo.png';
+  static const String profileRandomImage1 = 'assets/images/profile_random_image_1.jpeg';
+  static const String profileRandomImage2 = 'assets/images/profile_random_image_2.jpeg';
+  static const String profileRandomImage3 = 'assets/images/profile_random_image_3.jpeg';
+  static const String profileRandomImage4 = 'assets/images/profile_random_image_4.jpeg';
+  static const String profileRandomImage5 = 'assets/images/profile_random_image_5.jpeg';
+
+
 }

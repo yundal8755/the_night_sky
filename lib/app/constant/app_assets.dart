0dd94e92733c6a6a054370dd3226fa4a1fde401f
yundal <soneymarr@gmail.com>
2024-03-29 13:55:04 +0900

    chore: 로컬 에셋 경로 정의 모듈 추가

---
+++ lib/app/constant/app_assets.dart
@@ -0,0 +1,20 @@
+abstract class AppAssets {
+  //! icons
+  static const String replyDefault20 = 'assets/icons/message_pressed_20.svg';
+  static const String homeDefault32 = 'assets/icons/home_default_32.svg';
+  static const String homePressed32 = 'assets/icons/home_pressed_32.svg';
+  static const String postDefault32 = 'assets/icons/post_default_32.svg';
+  static const String micDefault40 = 'assets/icons/mic_default_40.svg';
+  static const String likeDefault24 = 'assets/icons/like_default_24.svg';
+  static const String chatDefault32 = 'assets/icons/chat_default_32.svg';
+  static const String chatPressed32 = 'assets/icons/chat_pressed_32.svg';
+  static const String playDefault56 = 'assets/icons/play_default_56.svg';
+  static const String playDefault32 = 'assets/icons/play_default_32.svg';
+  static const String pauseDefault56 = 'assets/icons/pause_default_56.svg';
+  static const String pauseDefault32 = 'assets/icons/pause_default_32.svg';
+
+  //! Images
+  static const String basicProfileImage = 'assets/image/basicProfileImage.png';
+  static const String googleLogo = 'assets/image/google_logo.png';
+  static const String appleLogo = 'assets/image/apple_logo.png';
+}

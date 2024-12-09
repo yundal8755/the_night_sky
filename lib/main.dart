4c534dec4d732389009a85cbb133426a015f7f6e
yundal <soneymarr@gmail.com>
2024-12-10 08:05:00 +0900

    chore : FCM 설정 (iOS)

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:everyones_tone/app/utils/edit_profile_manager.dart';
 import 'package:everyones_tone/app/utils/record_status_manager.dart';
 import 'package:everyones_tone/presentation/pages/bottom_nav_bar_page.dart';
 import 'package:everyones_tone/presentation/pages/login/login_view_model.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -14,20 +15,51 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  setupLocator(); // 의존성 등록 호출
+  setupLocator();
 
   await Future.delayed(const Duration(microseconds: 300));
   FlutterNativeSplash.remove();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // iOS 알림 권한 요청
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  String? fcmToken = await FirebaseMessaging.instance.getToken();
+  debugPrint('===== FCM TOKEN : $fcmToken =====');
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage? message) {
+    if (message != null && message.notification != null) {
+      debugPrint(
+          'Foreground 알림 수신: ${message.notification!.title} - ${message.notification!.body}');
+    }
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? message) {
+    if (message != null && message.notification != null) {
+      debugPrint(
+          'Background 알림 수신: ${message.notification!.title} - ${message.notification!.body}');
+    }
+  });
+
+  FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage? message) {
+    if (message != null && message.notification != null) {
+      debugPrint(
+          'Terminate 알림 수신: ${message.notification!.title} - ${message.notification!.body}');
+    }
+  });
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => RecordStatusManager()),
         ChangeNotifierProvider(create: (_) => EditProfileManager()),
         ChangeNotifierProvider(create: (_) => AudioPlayProvider()),
-        ChangeNotifierProvider(create: (_) => EditProfileManager()),
         ChangeNotifierProvider(create: (_) => FirestoreUserProvider()),
         ChangeNotifierProvider(create: (_) => LoginViewModel()),
       ],
